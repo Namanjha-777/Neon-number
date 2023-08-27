f092e69e96b46c9bca836c35b6e562e28d2b0fd0
Namanjha-777 <143312876+Namanjha-777@users.noreply.github.com>
2023-08-27 13:31:57 +0530

    Add files via upload

---
+++ NeonNumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class NeonNumber{
+    public static void main(String[] args){
+        int n,
+        sqr = 1,
+        sum = 0, r;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number=");
+        n = sc.nextInt();
+        sqr = n * n;
+        while (sqr > 0){
+            r = sqr % 10;
+            sum = sum + r;
+            sqr = sqr / 10;
+        }if (n == sum){
+            System.out.println("Neon Number");
+        }else{
+            System.out.println("Not Neon Number");
+        }
+    }
+}
